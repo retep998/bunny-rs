9ee596f1fa382848ae802e8f4e60432795116977
Peter Atashian <retep998@gmail.com>
2016-08-31 04:01:45 -0400

    Improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-//! Gives you a bunny.
+// Copyright © 2016 Peter Atashian
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// All files in the project carrying such notice may not be copied, modified, or distributed
+// except according to those terms.
+//! This crate provides lagomorphs.
 
-/// Bunny
+/// Acquires and returns a bunny.
 pub fn bunny() -> &'static str {
     "🐇"
 }
 
-/// Rabbit
-///
-/// Alternate name for bunnies.
+/// Acquires and returns a rabbit.
 pub fn rabbit() -> &'static str {
     "🐰"
 }
